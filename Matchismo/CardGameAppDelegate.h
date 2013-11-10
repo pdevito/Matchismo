d37ade0e7013ee1684249188093c5e1d7c91ef4c
kephas96 <ulme@Helmsdeep.local>
2013-11-10 18:39:08 -0500

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Peter DeVito on 11/10/13.
+//  Copyright (c) 2013 Peter DeVito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Peter DeVito on 11/10/13.
+//  Copyright (c) 2013 Peter DeVito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
